a875b9dc87710a59103255fe8ea7fcde6e4a5694
ConanXie <xjj416@gmail.com>
2015-10-13 19:06:59 +0800

    game

--- public/js/game/Canvas.js
+++ public/js/game/Canvas.js
@@ -19,11 +19,50 @@ define(function () {
     this.height = height*2;
   };
   Canvas.prototype.drawStart = function () {
-    var cxt = this.cxt;
+
+    /*
+    point 下方等边三角形的中心
+    h 小三角形的高
+    sT 根号3
+    */
+    
+    var cxt = this.cxt,
+        width = this.width,
+        height = this.height,
+        point = 300,
+        h = 70,
+        sT = Math.sqrt(3);
+
+    cxt.beginPath();
     cxt.fillStyle = 'rgb(0, 0, 0)';
-    cxt.fillRect(0, 0, this.width, this.height);
+    cxt.fillRect(0, 0, width, height);
+    cxt.closePath();
+    cxt.beginPath();
     cxt.fillStyle = 'rgb(255, 0, 0)';
-    cxt.arc(this.width/2, this.height/2, 150, 0, Math.PI*2, true);
+    cxt.arc(width/2, height/2, 150, 0, Math.PI*2, true);
+    cxt.closePath();
+    cxt.fill();
+
+    cxt.beginPath();
+    cxt.fillStyle = '#e91e63';
+    cxt.moveTo(width/2, height-point);
+    cxt.lineTo(width/2+h*sT, height-point-h);
+    cxt.lineTo(width/2-h*sT, height-point-h);
+    cxt.closePath();
+    cxt.fill();
+    cxt.beginPath();
+    cxt.fillStyle = '#9c27b0';
+    cxt.moveTo(width/2, height-point);
+    cxt.lineTo(width/2, height-point+h*sT);
+    cxt.lineTo(width/2-h*sT, height-point-h);
+    cxt.closePath();
+    cxt.fill();
+    cxt.beginPath();
+    cxt.fillStyle = '#009688';
+    cxt.moveTo(width/2, height-point);
+    cxt.lineTo(width/2, height-point+h*sT);
+    cxt.lineTo(width/2+h*sT, height-point-h);
+    cxt.closePath();
     cxt.fill();
   };
   return new Canvas(canvas);
